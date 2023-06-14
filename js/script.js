5211b39365f29b5713fd9d6ab5a2d4eac54e9f31
Olexander <alex.pilyavskiy03@gmail.com>
2023-06-14 19:26:36 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-"use strict"
+"use strict";
 
-const reviewsSwiper = document.querySelector('.swiper-reviews');
+const reviewsSwiper = document.querySelector(".swiper-reviews");
 
 if (reviewsSwiper) {
-	const swiper = new Swiper('.swiper-reviews', {
-		// Optional parameters
-		autoHeight: true,
-		loop: true,
-		// If we need pagination
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true
-		},
-	});
+  const swiper = new Swiper(".swiper-reviews", {
+    autoHeight: true,
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
